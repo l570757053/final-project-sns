9d56219998e8fcfcf44578746e828de152b3408a
Administrator <928810957@qq.com>
2022-04-28 19:49:06 +0800

    更新个人主页部分

--- blogger/views.py
+++ blogger/views.py
@@ -14,7 +14,7 @@ def index(request):
         f = models.Follow.objects.filter(fans=request.user).values_list('following', flat=True)
         msgs = models.Message.objects.select_related('user').filter(Q(user__in=f) | Q(user=request.user))
     else:
-        msgs = models.Message.objects.select_related('user').all()
+        msgs = models.Message.objects.select_related('user').filter(show=True)
     return render(request, "index.html", {
         'msgs': msgs
     })
@@ -63,8 +63,8 @@ def register(request):
         try:
             user = User.objects.create_user(username, email, password)
             user.ProfilePhoto = picture
-            notice = models.Notice(user=request.user)
-            background = models.Background(user=request.user)
+            notice = models.Notice(user=user)
+            background = models.Background(user=user)
             user.save()
             notice.save()
             background.save()
@@ -82,8 +82,10 @@ def publish(request):
     content = request.POST["content"]
     h = request.POST["h"]
     picture = request.FILES.get("picture")
+    # 公开
     if h == "n":
         show = True
+    # 粉丝可见
     else:
         show = False
     msg = models.Message(user=request.user, content=content, show=show, pic=picture)
@@ -92,10 +94,23 @@ def publish(request):
 
 
 def showuser(request, u_id):
-    user = request.user
-    msgs = models.Message.objects.filter(user=u_id)
-    return render(request, "user.html", {
-        'user': user,
-        'msgs': msgs,
-        'c_style': "col-sm-offset-3"
-    })
+    if request.method == "GET":
+        user = models.User.objects.get(username=u_id)
+        ctx = {'c_style': "col-sm-offset-3", 'user': user}
+        if request.user == user:
+            ctx['flag'] = False
+            msgs = models.Message.objects.filter(user=u_id)
+            ctx['msgs'] = msgs
+        else:
+            msgs = models.Message.objects.filter(user=u_id, show=True)
+            ctx['msgs'] = msgs
+            ctx['flag'] = True
+            fol = models.Follow.objects.filter(following=user, fans=request.user)
+            if fol:
+                ctx['btn_class'] = "btn btn-success"
+            else:
+                ctx['btn_class'] = "btn btn-default"
+        return render(request, "user.html", ctx)
+    else:
+        # TODO: 需要填充关注和修改签名，h的值为fol是关注，cha则是修改
+        return HttpResponseRedirect("/user/"+u_id)
